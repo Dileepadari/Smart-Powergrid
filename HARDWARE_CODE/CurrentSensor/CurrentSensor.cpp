1af163317e689aee4f4ca46a7b762217095c39a4
Asish Bharadwaj Chodavarapu <119788331+asish-bharadwaj@users.noreply.github.com>
2023-05-29 23:25:49 +0530

    latest CurrentSensor.cpp

--- HARDWARE_CODE/CurrentSensor/CurrentSensor.cpp
+++ HARDWARE_CODE/CurrentSensor/CurrentSensor.cpp
@@ -3,11 +3,16 @@ CurrentSensor::CurrentSensor(int CurrentPin)
 {
   inputPin=CurrentPin;
 }
+float CurrentSensor::getBaseValue(int base_pin)
+{
+   base_voltage=analogRead(base_pin)*3.3/4095.0;
+   return base_voltage;
+}
 float CurrentSensor::getValueinA()
 {
-  float volts=analogRead(inputPin)/1200.0;
+  float volts=analogRead(inputPin)*3.3/4095.0;
   Serial.println(volts);
-  float curr_in_A=(volts-2.5)*1000/185.0;
+  float curr_in_A=(volts-base_voltage)*1000/185.0;
   if(curr_in_A<0)
   {
     return curr_in_A*-1;
