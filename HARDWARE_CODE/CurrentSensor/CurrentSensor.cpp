ab1f06cef48a147a622bb79c89fe8bc6fe4ef518
Ishan Gupta <ishanguptajpr@gmail.com>
2023-05-29 15:29:36 +0530

    Changed scaling factor for voltage

--- HARDWARE_CODE/CurrentSensor/CurrentSensor.cpp
+++ HARDWARE_CODE/CurrentSensor/CurrentSensor.cpp
@@ -5,9 +5,9 @@ CurrentSensor::CurrentSensor(int CurrentPin)
 }
 float CurrentSensor::getValueinA()
 {
-  float volts=analogRead(inputPin)*3.3/4095;
+  float volts=analogRead(inputPin)/1200.0;
   Serial.println(volts);
-  float curr_in_A=(volts-2.5)*1000/185;
+  float curr_in_A=(volts-2.5)*1000/185.0;
   if(curr_in_A<0)
   {
     return curr_in_A*-1;
